ebb291c0b95e7ef580cd27f091a85f641f4ee9d2
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 20:10:49 +0000

    use verbose completions for zsh and fish descriptions

--- completions/mpt.fish
+++ completions/mpt.fish
@@ -1,2 +1,2 @@
 # fish completion for mpt (generated via go-flags)
-complete -c mpt -a '(GO_FLAGS_COMPLETION=1 mpt (commandline -cop) 2>/dev/null)'
+complete -c mpt -a '(GO_FLAGS_COMPLETION=verbose mpt (commandline -cop) 2>/dev/null | string replace -r "\\s+# " "\t")'
