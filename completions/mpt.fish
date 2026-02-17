e9fc6acd884e9fc454150d933db32e22dd3e1576
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 19:39:17 +0000

    Add shell completions for bash, zsh, and fish

---
+++ completions/mpt.fish
@@ -0,0 +1,2 @@
+# fish completion for mpt (generated via go-flags)
+complete -c mpt -a '(GO_FLAGS_COMPLETION=1 mpt (commandline -cop) 2>/dev/null)'
---
+++ completions/mpt.zsh
@@ -0,0 +1,15 @@
+#compdef mpt
+
+# zsh completion for mpt (generated via go-flags)
+_mpt() {
+    local IFS=$'\n'
+    local -a completions
+    completions=($(GO_FLAGS_COMPLETION=1 "${words[1]}" "${(@)words[2,$CURRENT]}" 2>/dev/null))
+    if (( ${#completions} )); then
+        compadd -- "${completions[@]}"
+    else
+        _files
+    fi
+}
+
+_mpt "$@"
